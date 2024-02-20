1013c0bdb8ed63e2204856466d60488d22bc2b32
Sadanand012 <sadamare8408@gmail.com>
2024-02-20 23:30:54 +0530

    socket io

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const { Server } = require('socket.io');
+
+const app = express();
+const serv =  http.createServer(app);
+const io = new Server(serv);
+
+//socket io
+
+io.on('connection', (socket) => {
+// console.log("A new user has connected", socket.id);
+socket.on('user-message', (message) => {
+    // console.log("A new User message", message);
+    io.emit('message',message);
+})
+})
+
+app.use(express.static("./public"));
+
+app.get('/', (req, res) => {
+    return res.sendFile("./public/index.html")
+})
+serv.listen(9000, () => console.log(`server runnig on port 9000`));
